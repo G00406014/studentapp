de32790967bd39e074bd588c19b5f385fdbf74d9
Alex Cronin <G00406014@atu.ie>
2022-10-18 14:13:02 +0100

    ADDED NEW FILE CALLED STUDENT1

---
+++ src/main/java/ie/atu/lab5/Student1.java
@@ -0,0 +1,34 @@
+package ie.atu.lab5;
+
+import java.util.Scanner;
+
+public class Student1 {
+    private double name;
+    private double email;
+    private double course;
+
+
+    public double getName() {
+        return name;
+    }
+
+    public void setName(double name) {
+        this.name = name;
+    }
+
+    public double getEmail() {
+        return email;
+    }
+
+    public void setEmail(double email) {
+        this.email = email;
+    }
+
+    public double getCourse() {
+        return course;
+    }
+
+    public void setCourse(double course) {
+        this.course = course;
+    }
+}
--- src/main/java/ie/atu/lab5/studentapp.java
+++ src/main/java/ie/atu/lab5/studentapp.java
@@ -5,9 +5,17 @@ import java.util.Scanner;
 public class studentapp {
 
     public static void main(String[] args) {
-        System.out.println("please enterthe width of the rectangle");
+        System.out.println("please enter student name");
         Scanner input = new Scanner(System.in);
-        double width = input.nextDouble();
-        System.out.println("you entered : " + width);
+        double name = input.nextDouble();
+        System.out.println("you entered : " + name);
+
+        System.out.println("please enter the students email");
+        double email = input.nextDouble();
+        System.out.println("you entered : " + email);
+
+        System.out.println("please enter the students course");
+        double course = input.nextDouble();
+        System.out.println("you entered : " + course);
     }
 }
