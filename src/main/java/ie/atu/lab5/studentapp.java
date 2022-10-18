42812d3e4e41a019fc39fe8bbd0a4e8e4eea5599
Alex Cronin <G00406014@atu.ie>
2022-10-18 13:42:32 +0100

    start of project for student app

--- src/main/java/ie/atu/lab5/studentapp.java
+++ src/main/java/ie/atu/lab5/studentapp.java
@@ -1,4 +1,13 @@
 package ie.atu.lab5;
 
+import java.util.Scanner;
+
 public class studentapp {
+
+    public static void main(String[] args) {
+        System.out.println("please enterthe width of the rectangle");
+        Scanner input = new Scanner(System.in);
+        double width = input.nextDouble();
+        System.out.println("you entered : " + width);
+    }
 }
